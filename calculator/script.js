f49bae67a676b00a0cf6da239d61e84feac8794d
Priyanka Bait <priyankabait04@gmail.com>
2026-05-22 14:45:51 +0530

    Initial CodSoft Projects Commit

---
+++ calculator/script.js
@@ -0,0 +1,41 @@
+const display =
+document.getElementById("display");
+
+/* Append Value */
+
+function appendValue(value){
+
+    display.value += value;
+}
+
+
+
+function clearDisplay(){
+
+    display.value = "";
+}
+
+
+
+function deleteLast(){
+
+    display.value =
+    display.value.slice(0,-1);
+}
+
+
+
+function calculateResult(){
+
+    try{
+
+        display.value =
+        eval(display.value);
+
+    }
+
+    catch{
+
+        display.value = "Error";
+    }
+}
